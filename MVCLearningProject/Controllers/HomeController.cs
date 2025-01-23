4e2ba5041c48af69578cb74be9d3e02ae3123437
Ruhi Garga <gargaruhi9@gmail.com>
2025-01-23 16:41:22 +0000

    Add MVC Controllers

---
+++ MVCLearningProject/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCLearningProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello World!";
+        }
+        public string Error()
+        {
+            return "I have an error here.";
+        }
+    }
+}
