38493e8bb9ae7ebaa28712348b5e351c9911d431
Ruhi Garga <gargaruhi9@gmail.com>
2025-01-23 18:59:28 +0000

    Added bootstrap
    Added bootstrap

--- MVCLearningProject/Program.cs
+++ MVCLearningProject/Program.cs
@@ -6,7 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+app.UseStaticFiles();
+
 app.UseRouting();
+
 app.MapControllerRoute(
     name: "default",
     pattern:"{controller=Home}/{action=Index}/{id?}");
