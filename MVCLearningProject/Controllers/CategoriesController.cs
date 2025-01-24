364b6e22b0019036c8bd63a7bb4f5934cfb93c63
Ruhi Garga <gargaruhi9@gmail.com>
2025-01-24 17:03:24 +0000

    Model Binding

--- MVCLearningProject/Controllers/CategoriesController.cs
+++ MVCLearningProject/Controllers/CategoriesController.cs
@@ -12,7 +12,8 @@ namespace MVCLearningProject.Controllers
         }
         public IActionResult Edit(int? id)
         {
-            var category = new Category { CategoryId = id.HasValue ? id.Value : 0 };
+            var category = CategoriesRepository.GetCategory(id.HasValue ? id.Value : 0);
+            //var category = new Category { CategoryId = id.HasValue ? id.Value : 0 };
            return View(category);
         }
     }
